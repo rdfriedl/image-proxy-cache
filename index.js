88a34ac8135132d78e565e8a303a1644daf64b76
Robert Friedl <robert.friedl@equipmentshare.com>
2022-04-14 16:23:48 -0500

    ignore errors from weserv format code

--- index.js
+++ index.js
@@ -6,87 +6,107 @@ const crypto = require("crypto");
 const express = require("express");
 
 const userAgent =
-	"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.88 Safari/537.36";
+  "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.88 Safari/537.36";
 const fetchImage = async (imageUrl) => {
-	return Promise.race([
-		axios.get(imageUrl, {
-			responseType: "arraybuffer",
-			headers: { "user-agent": userAgent },
-		}),
-		axios.get("https://images.weserv.nl?url=" + encodeURIComponent(imageUrl), {
-			responseType: "arraybuffer",
-			headers: { "user-agent": userAgent },
-		}),
-	]);
+  return new Promise((res, rej) => {
+    let complete = false;
+    axios
+      .get(imageUrl, {
+        responseType: "arraybuffer",
+        headers: { "user-agent": userAgent },
+      })
+      .then((data) => {
+        if (!complete) {
+          complete = true;
+          res(data);
+        }
+      })
+      .catch((err) => {
+        if (!complete) {
+          complete = true;
+          rej(err);
+        }
+      });
+    axios
+      .get("https://images.weserv.nl?url=" + encodeURIComponent(imageUrl), {
+        responseType: "arraybuffer",
+        headers: { "user-agent": userAgent },
+      })
+      .then((data) => {
+        if (!complete) {
+          complete = true;
+          res(data);
+        }
+      })
+      .catch((err) => {});
+  });
 };
 
 const cachePathEnv = process.env.CACHE_DIR || "./cache";
-const cacheDir = path.isAbsolute(cachePathEnv)
-	? cachePathEnv
-	: path.join(process.cwd(), cachePathEnv);
+const cacheDir = path.isAbsolute(cachePathEnv) ? cachePathEnv : path.join(process.cwd(), cachePathEnv);
 
 const app = express();
 
 app.get("/", async (req, res) => {
-	if (!req.query.url) {
-		throw new Error("missing url param");
-	}
+  if (!req.query.url) {
+    throw new Error("missing url param");
+  }
 
-	const imageUrl = req.query.url;
-	const hash = crypto.createHash("md5").update(imageUrl).digest("hex");
-	const metadataPath = path.join(cacheDir, hash + ".json");
-	const cachePath = path.join(cacheDir, hash);
+  const imageUrl = req.query.url;
+  const hash = crypto.createHash("md5").update(imageUrl).digest("hex");
+  const metadataPath = path.join(cacheDir, hash + ".json");
+  const cachePath = path.join(cacheDir, hash);
 
-	try {
-		const metadata = fs.existsSync(metadataPath)
-			? JSON.parse(fs.readFileSync(metadataPath, { encoding: "utf-8" }))
-			: null;
+  try {
+    const metadata = fs.existsSync(metadataPath)
+      ? JSON.parse(fs.readFileSync(metadataPath, { encoding: "utf-8" }))
+      : null;
 
-		if (metadata && fs.existsSync(cachePath)) {
-			Object.keys(metadata.headers).forEach((name) => {
-				res.setHeader(name, metadata.headers[name]);
-			});
+    if (metadata && fs.existsSync(cachePath)) {
+      Object.keys(metadata.headers).forEach((name) => {
+        res.setHeader(name, metadata.headers[name]);
+      });
 
-			res.sendFile(cachePath);
-			return;
-		}
-	} catch (err) {
-		res.send("failed to read cache");
-		try {
-			console.log(`removing cache files for ${hash}`);
-			fs.rmSync(metadataPath);
-			fs.rmSync(cachePath);
-		} catch (e) {}
-		return;
-	}
+      res.sendFile(cachePath);
+      return;
+    }
+  } catch (err) {
+    res.send("failed to read cache");
+    try {
+      console.log(`removing cache files for ${hash}`);
+      fs.rmSync(metadataPath);
+      fs.rmSync(cachePath);
+    } catch (e) {}
+    return;
+  }
 
-	console.log(`Fetching ${imageUrl}`);
-	const imageRes = await fetchImage(imageUrl);
+  console.log(`Fetching ${imageUrl}`);
+  const imageRes = await fetchImage(imageUrl);
 
-	if (!imageRes.headers["content-type"].includes("image")) {
-		throw new Error("the url must point to an image");
-	}
+  if (!imageRes.headers["content-type"].includes("image")) {
+    throw new Error("the url must point to an image");
+  }
 
-	const headers = {
-		"content-type": imageRes.headers["content-type"],
-	};
-	const metadata = { headers };
+  const headers = {
+    "content-type": imageRes.headers["content-type"],
+  };
+  const metadata = { headers };
 
-	console.log(`Writing cache ${cachePath}`);
-	fs.writeFileSync(cachePath, imageRes.data);
-	fs.writeFileSync(metadataPath, JSON.stringify(metadata), {
-		encoding: "utf-8",
-	});
+  console.log(`Writing cache ${cachePath}`);
+  fs.writeFileSync(cachePath, imageRes.data);
+  fs.writeFileSync(metadataPath, JSON.stringify(metadata), {
+    encoding: "utf-8",
+  });
 
-	Object.keys(headers).forEach((name) => {
-		res.setHeader(name, headers[name]);
-	});
-	res.sendFile(cachePath);
+  Object.keys(headers).forEach((name) => {
+    res.setHeader(name, headers[name]);
+  });
+  res.sendFile(cachePath);
 });
 
 app.listen(3000);
 console.log("started server on 3000");
 
 process.on("SIGINT", () => {
-	process.exit();
+  process.exit();
 });
